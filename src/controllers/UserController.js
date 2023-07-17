d39b9596abb62e5e7089a5a855251942a909b355
Matheus <matheus.rodrigues2010@hotmail.com>
2023-07-16 23:12:13 -0300

    creating a users CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,69 @@ class UserController {
     try {
       const novoUser = await User.create(req.body);
 
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({ erros: e.errors.map((err) => err.message) });
+      return res.status(400).json({ erros: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (e) {
+      return res.status(400).json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ['ID não enviado.'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não existe.'] });
+      }
+
+      const newUser = await user.update(req.body);
+
+      return res.json(newUser);
+    } catch (e) {
+      return res.status(400).json({ erros: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ['ID não enviado.'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não existe.'] });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ erros: e.errors.map((err) => err.message) });
     }
   }
 }
